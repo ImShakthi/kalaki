8a4b9886c910a68f6ca36f2346bca2d20365ccd4
sakthivel <sakthi836800@gmail.com>
2018-01-20 19:26:58 +0530

    Added iFrame and intergated the BFFER nd Enhanced the look with BETA

--- target/classes/public/scripts/BaseHandlerUtil.js
+++ target/classes/public/scripts/BaseHandlerUtil.js
@@ -1,26 +1,42 @@
 var BaseHandlerUtil = {
 	init : function() {
-		$(document).delegate('#textbox', 'keydown', function(e) { 
-			  var keyCode = e.keyCode || e.which; 
+		console.log("init....");
+		try{
+			$(document).ready(function() {
+				
+				$('textarea').each(function(i, block) {
+				    hljs.highlightBlock(block);
+				  });
+				
+				
+				$("textarea").keydown(function(e) {
+				    if(e.keyCode === 9) { // tab was pressed
+				        // get caret position/selection
+				        var start = this.selectionStart;
+				            end = this.selectionEnd;
 
-			  if (keyCode == 9) { 
-			    e.preventDefault(); 
-			    var start = $(this).get(0).selectionStart;
-			    var end = $(this).get(0).selectionEnd;
+				        var $this = $(this);
 
-			    // set textarea value to: text before caret + tab + text after caret
-			    $(this).val($(this).val().substring(0, start)
-			                + "\t"
-			                + $(this).val().substring(end));
+				        // set textarea value to: text before caret + tab + text after caret
+				        $this.val($this.val().substring(0, start)
+				                    + "\t"
+				                    + $this.val().substring(end));
 
-			    // put caret at right position again
-			    $(this).get(0).selectionStart = 
-			    $(this).get(0).selectionEnd = start + 1;
-			  } 
+				        // put caret at right position again
+				        this.selectionStart = this.selectionEnd = start + 1;
+
+				        // prevent the focus lose
+				        return false;
+				    }
+				});
 			});
+		} catch(e){
+			alert("Error in adding listen" + e);
+			console.log("Error in adding listen", e);
+		}
 	},
 	onClick : function() {
-//		d3.selectAll("p").style("color", "#" + BaseHandlerUtil.getRandomColor());
+		//		d3.selectAll("p").style("color", "#" + BaseHandlerUtil.getRandomColor());
 	},
 	getRandomColor : function() {
 		var colorCode = "";
@@ -55,23 +71,73 @@ var BaseHandlerUtil = {
 			var css = document.getElementById('cssText').value;
 			var js = document.getElementById('jsText').value;
 
-			console.log("HTML ", html);
-			console.log("CSS ", css);
-			console.log("JS ", js);
-
-			var cssBuffer = " <style> " + css + " </style> ";
-			var jsBuffer = " <script type=\"text/javascript\"> " + js + " </script> ";
-
-			var buffer = "";
-			buffer = "<div> " + cssBuffer + jsBuffer + html + " </div> ";
-			console.log(buffer);
 			try {
 				evalJS = eval(js);
 			} catch (err) {
 				alert("Error JS block " + err);
 			}
 
-			document.getElementById('renderDiv').innerHTML = buffer;
+			var params = {};
+			params.css = css;
+			params.js = js;
+			params.html = html;
+
+			BaseHandlerUtil.appendToHead(params);
+			BaseHandlerUtil.appendToBody(params);
+
+		} catch (e) {
+			alert(e);
+			console.log(e);
+		}
+	},
+
+	appendToHead : function(params) {
+		try {
+			var renderDiv = document.getElementById("renderDiv");
+			var renderDOM = (renderDiv.contentDocument || renderDiv.contentWindow);
+
+			var head = renderDOM.head || renderDOM.getElementsByTagName('head')[0];
+
+			head.innerHTML = "";
+
+			var meta = document.createElement('meta');
+			meta['httpEquiv'] = 'Content-Type';
+			head.appendChild(meta);
+
+			var style = document.createElement('style');
+			style.type = 'text/css';
+			var css = params.css;
+			if (style.styleSheet) {
+				style.styleSheet.cssText = css;
+			} else {
+				style.appendChild(document.createTextNode(css));
+			}
+			head.appendChild(style);
+
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.textContent = params.js;
+			head.appendChild(script);
+
+		} catch (e) {
+			alert(e);
+			console.log(e);
+		}
+	},
+	appendToBody : function(params) {
+		try {
+			var renderDiv = document.getElementById("renderDiv");
+			var renderDOM = (renderDiv.contentDocument || renderDiv.contentWindow);
+
+			var body = renderDOM.body || renderDOM.getElementsByTagName('body')[0];
+
+			body.innerHTML = "";
+
+			var div = document.createElement('div');
+			div.id = 'mainDiv';
+			div.innerHTML = params.html;
+			body.appendChild(div);
+
 		} catch (e) {
 			alert(e);
 			console.log(e);
